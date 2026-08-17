f95f85714c051900a0c2ab01479f52d48ba3f725
Aluno em Vocação Nicolas santana <paivaester443@gmail.com>
2026-08-17 11:14:56 -0300

    uptade
    uptade

---
+++ modulo06/modulo06ativ01_crie_arquivo_esterfurukawa.py
@@ -0,0 +1,9 @@
+'''
+Como criar e ler um arquivo em txt:
+
+Para abrir use "arquivo = open("nome_do_arquivo.txt", "modo")
+
+Para ler use "arquivo = open("exemplo.txt", "r")
+conteudo = arquivo.read() 
+print(conteudo)"
+'''
