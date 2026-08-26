adff5641e3af2ae95db74534dbc934a16d074b06
Aluno Vocacao <paivaester443@gmail.com>
2026-08-26 09:11:08 -0300

    Update

---
+++ modulo02/modulo02ativ01_testes_basicos_esterfurukawa.py
@@ -0,0 +1,9 @@
+# Exibindo mensagens no console
+print("Olá, mundo!")
+print("Explorando o interpretador Python.")
+
+# Verificando tipos de dados com type()
+print("Tipo de 'Python':", type("Python"))
+print("Tipo de 42:", type(42))
+print("Tipo de 3.14:", type(3.14))
+print("Tipo de True:", type(True))
