e1cb6c4c075b4e5d8ae017b35ddb18e1123f7ea9
Aluno Vocacao <paivaester443@gmail.com>
2026-08-26 10:27:58 -0300

    Update

--- modulo02/modulo02ativ01_testes_basicos_esterfurukawa.py
+++ modulo02/modulo02ativ01_testes_basicos_esterfurukawa.py
@@ -1,9 +1,14 @@
-# Exibindo mensagens no console
-print("Olá, mundo!")
-print("Explorando o interpretador Python.")
 
-# Verificando tipos de dados com type()
-print("Tipo de 'Python':", type("Python"))
-print("Tipo de 42:", type(42))
-print("Tipo de 3.14:", type(3.14))
-print("Tipo de True:", type(True))
+print("Olá, mundo!")
+print("Explorando o Python!")
+
+
+texto = "Python"
+inteiro = 42
+decimal = 3.14
+booleano = True
+
+print("O tipo de 'texto' é:", type(texto))
+print("O tipo de 'inteiro' é:", type(inteiro))
+print("O tipo de 'decimal' é:", type(decimal))
+print("O tipo de 'booleano' é:", type(booleano))
--- modulo02/modulo02ativ03_intro_python_esterfurukawa.py
+++ modulo02/modulo02ativ03_intro_python_esterfurukawa.py
@@ -1,4 +1,4 @@
 
 nome = input("Por favor, digite o seu nome: ")
 
-print(f"Olá, {nome}! Seja muito bem-vindo(a) ao mundo da programação com Python!")
+print(f"Olá, {nome}! Seja muito bem-vindo(a) ao mundo da programação com Python! 🏴‍☠️")
