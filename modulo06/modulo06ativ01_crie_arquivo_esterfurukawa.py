31c78fd847641a63f47f84e8aef3e4b340ddf592
Aluno Vocacao <paivaester443@gmail.com>
2026-08-26 10:46:42 -0300

    update

--- modulo06/modulo06ativ01_crie_arquivo_esterfurukawa.py
+++ modulo06/modulo06ativ01_crie_arquivo_esterfurukawa.py
@@ -9,13 +9,10 @@ print(conteudo)"
 '''
 
 
-# 1 . Criando um arquivo TXT com o nome "nome_arquivo.txt" 
-# e escrevendo algumas informações nele.
+
 nome_arquivo = "dados_arquivo.txt"
 
 
-# 2 . Conteúdo a ser escrito no arquivo
-# --- ESCRITA ---
 conteudo = [
     "Ester Furukawa;16 anos;02899-000;947541;esterfurukawa@mail.com\n",
     "Isabelly Castelo;17 anos;057193-000;978786;isabellycastelo@mail.com\n",
@@ -23,15 +20,13 @@ conteudo = [
 ]
 
 
-# 3 . Escrevendo no arquivo
+
 with open(nome_arquivo, "w", encoding="utf-8") as arquivo:
     arquivo.writelines(conteudo)
 print(f"✅ Arquivo '{nome_arquivo}' criado e escrito com sucesso!")
 
 
 
-# 4 . Lendo o conteúdo do arquivo
-# --- LEITURA ---
 print("\n--- Lendo o conteúdo do arquivo TXT ---")
 with open(nome_arquivo, "r", encoding="utf-8") as arquivo:
     texto = arquivo.read()
